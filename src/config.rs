31e5c5b71cc1fdf54b5943b502e667f605f7be58
g-cz2025 <541717827@qq.com>
2025-01-31 23:30:27 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["hw.dl999.cn"];
+pub const RS_PUB_KEY: &str = "38Ea3nNJ8oo9LG+QThVm5zj94VKw3qgrSSKv5x2mn2w=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
